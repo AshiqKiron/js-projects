412f34f1a877bcccb532b64813d80325e3130f35
Ashiq <ashiq.kiron@gmail.com>
2018-01-02 12:13:16 +0600

    Show Navbar on scrolling down

---
+++ Vanilla JS show nav on scroll/scripts.js
@@ -0,0 +1,11 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
