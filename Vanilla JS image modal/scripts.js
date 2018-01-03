f2f9e03845fb2397da5fc715c5ba5a9aeeca236e
Ashiq <ashiq.kiron@gmail.com>
2018-01-03 15:18:34 +0600

    Vanilla JS Image Modal with caption

--- Vanilla JS image modal/scripts.js
+++ Vanilla JS image modal/scripts.js
@@ -1,31 +1,43 @@
-//grab all elements
-const imgMain = document.getElementById('myImg');
-const modalWindow = document.getElementById('myModal');
-const closeModal =  document.getElementsByClassName('close')[0];
-const modalContent = document.getElementById('img01');
-const modalCaption = document.getElementById('caption');
+/*
+<div class="row">
+  <div class="column">
+    <img src="index_02.jpg" alt="Nature" style="width:100%" onclick="myFunction(this);">
+  </div>
+  <div class="column">
+    <img src="index_01.jpg" alt="Fjords" style="width:100%" onclick="myFunction(this);">
+  </div>
+</div>
 
-//show modal image, caption and window
-function showModal() {
-	modalWindow.style.display = "block";
-	modalContent.src = this.src;
-	modalCaption.innerHTML = this.alt;
+<div class="container">
+  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+  <img id="expandedImg" style="width:100%">
+  <div id="imgtext"></div>
+</div>
+*/
+
+const imgSource = document.querySelectorAll('img');
+const showImg = document.getElementById('expandedImg');
+const imgTxt = document.getElementById('imgtext');
+
+function imgToggle() {
+	showImg.parentElement.style.display = "block";
+	showImg.src = this.src;
+	imgTxt.innerHTML = this.alt;
 }
 
-imgMain.addEventListener('click', showModal);
+imgSource.forEach(image => image.addEventListener('click', imgToggle));
 
-//clicking close icon will close modal window 
-function hideModal() {
-	modalWindow.style.display = "none";
-}
 
-closeModal.addEventListener('click', hideModal);
-
-//clicking anywhere will close the modal window
-function clickClose(event) {
-    // if modal initiated, clicking anywhere will hide it 
-    if (event.target == modalWindow) {
-        modalWindow.style.display = "none";
-    }
-}
-window.addEventListener('click', clickClose);
+/*
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}*/
