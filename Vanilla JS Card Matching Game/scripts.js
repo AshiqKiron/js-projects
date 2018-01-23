8dbc27b7c041c14318168f0e713d242a3ed1a2f3
Ashiq <ashiq.kiron@gmail.com>
2018-01-23 11:14:22 +0600

    Vanilla JS Card Matching game

--- Vanilla JS Card Matching Game/scripts.js
+++ Vanilla JS Card Matching Game/scripts.js
@@ -1,4 +1,17 @@
+/*
+1.Cards are to be shuffled on load or restart
+2.Game should know how to handle matched and unmatched cards
+3.Game should display the current number of moves a user has made
+4.The game should show a star rating (from 1–3) that reflects the player’s performance based on the number of moves made. 
+5.When a player starts a game, a displayed timer should also start and once the player wins the game, the timer stops.
+6.A restart button should allow the player reset the game board, the timer, and the star rating.
+7.A congratulations modal should appear when the player wins while showing a button to play again and modal should show: How much time it took, and star rating.
+*/
+
+
 //https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript
+
+
 
 // cards array holds all cards
 let card = document.getElementsByClassName("card");
