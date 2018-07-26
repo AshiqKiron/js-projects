2d42fc930487eab860749876c4c193f1e0c1205a
Ashiq <ashiq.kiron@gmail.com>
2018-07-26 14:39:29 +0600

    React JS Counter

---
+++ React JS Counter/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    //Set the initial state of the component
+    //Point the global context of this keyword.
+    this.state = {
+      counter: 0,
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(state => ({ counter: state.counter + 1 }));
+  }
+
+  onDecrement() {
+    this.setState(state => ({ counter: state.counter - 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.counter}</p>
+
+        <button onClick={this.onIncrement} type="button">Increment</button>
+        <button onClick={this.onDecrement} type="button">Decrement</button>
+      </div>
+    );
+  }
+}
+
+/* Refactored code
+class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+
+  onIncrement = () => {
+    this.setState(state => ({ counter: state.counter + 1 }));
+  }
+
+  onDecrement = () => {
+    this.setState(state => ({ counter: state.counter - 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.counter}</p>
+
+        <button onClick={this.onIncrement} type="button">Increment</button>
+        <button onClick={this.onDecrement} type="button">Decrement</button>
+      </div>
+    );
+  }
+}
+*/
+
+export default Counter;
